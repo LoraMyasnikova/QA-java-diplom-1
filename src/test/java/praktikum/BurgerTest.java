b9534a990b7fdc44ba238f692411669d1292522e
Lora Myasnikova <loramya@yandex.ru>
2022-04-24 21:32:48 +0700

    Add BurgerTest

---
+++ src/test/java/praktikum/BurgerTest.java
@@ -0,0 +1,63 @@
+package praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BurgerTest {
+
+    Bun bun;
+    Ingredient ingredient;
+
+    private final int ingredientIndexToRemove = 0;
+    private final int ingredientIndexMoveFrom = 0;
+    private final int ingredientIndexMoveTo = 1;
+    private final float expectedPrice = 20F;
+    private final String expectedReceipt = "This is expected receipt";
+
+    @Mock
+    Burger burger;
+
+    @Test
+    public void setBuns() {
+        burger.setBuns(bun);
+        Mockito.verify(burger).setBuns(bun);
+    }
+
+    @Test
+    public void addIngredient() {
+        burger.addIngredient(ingredient);
+        Mockito.verify(burger).addIngredient(ingredient);
+    }
+
+    @Test
+    public void removeIngredient() {
+        burger.removeIngredient(ingredientIndexToRemove);
+        Mockito.verify(burger).removeIngredient(ingredientIndexToRemove);
+    }
+
+    @Test
+    public void moveIngredient() {
+        burger.moveIngredient(ingredientIndexMoveFrom, ingredientIndexMoveTo);
+        Mockito.verify(burger).moveIngredient(ingredientIndexMoveFrom, ingredientIndexMoveTo);
+    }
+
+    @Test
+    public void getPrice() {
+        Mockito.when(burger.getPrice()).thenReturn(20F);
+        float actualResults = burger.getPrice();
+        assertEquals(expectedPrice, actualResults, 0.001F);
+    }
+
+    @Test
+    public void getReceipt() {
+        Mockito.when(burger.getReceipt()).thenReturn(expectedReceipt);
+        String actualResult = burger.getReceipt();
+        assertEquals(actualResult, expectedReceipt);
+    }
+}
